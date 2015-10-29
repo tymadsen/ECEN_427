3eb7401dd426c1f8f88c70029d0bd14bb3c3c274
tymadsen <ty.a.madsen@gmail.com>
2015-10-28 22:20:34 -0600

    new sounds

--- Sound Conversion/convertWav.py
+++ Sound Conversion/convertWav.py
@@ -4,18 +4,21 @@ import wave
 import struct
 
 def main():
-	print 'hello there'
-	name = sys.argv[2]
+	for i in sys.argv[1:]:
+		outfile(i)
+
+def outfile(name):
+	# print 'hello there'
+	# name = sys.argv[2]
 	
-	infile = wave.open(sys.argv[1], 'r')
+	infile = wave.open('../Wavs/AudioFiles/'+name+'.wav', 'rb')
 	outfile = codecs.open(name+'.c', 'w')
 
 	outfile.write("int "+name+"_soundData[] = {");
 
 	(nchannels, sampwidth, framerate, nframes, comptype, compname) = infile.getparams ()
-
-	for x in xrange(0,nframes):
-		# print frames
+	print sampwidth
+	for x in xrange(1,nframes):
 		outfile.write(str(ord(infile.readframes(1))))
 		if x < nframes:
 			outfile.write(", ")
